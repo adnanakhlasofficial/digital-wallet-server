2ef1e6cce09bcf369b0ae5bcb8721e4e99ef343d
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2025-09-08 00:05:06 +0600

    refactor: centralize shutdown handling for signals and runtime errors (SIGINT, SIGTERM, unhandledRejection, uncaughtException)

--- src/server.ts
+++ src/server.ts
@@ -44,3 +44,51 @@ let server: Server;
     console.log(error);
   }
 })();
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal received... Server shutting down..");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT signal received... Server shutting down..");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled rejection detected... Server shutting down..", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected... Server shutting down..", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
