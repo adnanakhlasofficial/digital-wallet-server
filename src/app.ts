0a124e716fdf7da02857d347d0c5472d39d77819
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2025-09-15 04:17:38 +0600

    chore(cookie): update frontend cookie settings for improved session handling

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,10 @@ import { globalErrorHandler } from "./middlewares/global-error-handler";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 app.use(cookieParser());
 app.use("/api/v1", router);
 
